10b2656f896428e1ceb7ff70e85e19f769fb0e4a
Eyudica <e.yudica@alumno.um.edu.ar>
2026-02-15 12:06:16 -0400

    Add 3 process working

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from multiprocessing import Process, Queue
+import os
 # from servers.ssh_server import run_ssh_server
 # from servers.http_server import run_http_server
 from ssh_server import run_ssh_server
@@ -13,8 +14,13 @@ def run_ssh(q):
 def run_http(q):
     print("[+] HTTP process started")
     asyncio.run(run_http_server(q))
-
+def delete_files():
+    if os.path.exists("http_log.json"):
+        os.remove("http_log.json")
+    if os.path.exists("ssh_log.json"):
+        os.remove("ssh_log.json")
 if __name__ == "__main__":
+    delete_files()
     q = Queue()
 
     ssh = Process(target=run_ssh, args=(q,))
--- parser.py
+++ parser.py
@@ -1,10 +1,20 @@
+import json
 def parser(q):
     while True:
         line = q.get()
-        if line['server'] == "SSH":
-            print("Conexion ssh")
-        if line['server'] == "HTTP":
-            print("Conexion http")
+        server=line["server"]
+        if server == "SSH":
+            filename="ssh_log.json"
+            save_to_file(line,filename)
+        elif server == "HTTP":
+            filename="http_log.json"
+            save_to_file(line,filename)
         else:
             print("Conexion desconocida")
+
+def save_to_file(data,filename):
+    with open(filename, "a") as f:
+        json.dump(data, f)
+        f.write("\n")
+    
         
